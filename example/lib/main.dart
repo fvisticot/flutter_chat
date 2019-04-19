94bc20c1942dedb32bd324416056c67c47bc60aa
MattisBrizardOAB <mattis.brizard@orange.com>
2019-04-19 11:11:42 +0200

    Refactored & updated dependencies (fixes camera crash)Refactored & updated dependencies (fixes camera crash)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,11 +13,13 @@ class SimpleBlocDelegate extends BlocDelegate {
   @override
   void onTransition(Transition transition) {
     print(transition);
+    super.onTransition(transition);
   }
 
   @override
   void onError(Object error, StackTrace stacktrace) {
     print(error);
+    super.onError(error, stacktrace);
   }
 }
 
