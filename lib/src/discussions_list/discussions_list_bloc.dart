62e1ead1b268bf68f6bf81458f48c00d377a9320
MattisBrizardOAB <mattis.brizard@orange.com>
2019-04-29 16:46:23 +0200

    Refactored with group management bloc

--- lib/src/discussions_list/discussions_list_bloc.dart
+++ lib/src/discussions_list/discussions_list_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:flutter_chat/src/group_management/group_management.dart';
 import 'package:flutter_chat/src/repositories/firebase_repository.dart';
 
 import 'discussions_list.dart';
@@ -8,10 +9,13 @@ import 'discussions_list.dart';
 class DiscussionsListBloc
     extends Bloc<DiscussionsListEvent, DiscussionsListState> {
   FirebaseRepository firebaseRepository;
+  GroupManagementBloc groupManagementBloc;
   String userId;
 
-  DiscussionsListBloc(this.firebaseRepository, this.userId)
+  DiscussionsListBloc(
+      this.firebaseRepository, this.groupManagementBloc, this.userId)
       : assert(firebaseRepository != null),
+        assert(groupManagementBloc != null),
         assert(userId != null);
 
   @override
@@ -22,7 +26,8 @@ class DiscussionsListBloc
       DiscussionsListEvent event) async* {
     if (event is GetDiscussionsList) {
       yield DiscussionsLoading();
-      Map<String, String> discussions;
+      Map<String, dynamic> discussions =
+          await firebaseRepository.getUserDiscussions(userId);
       yield DiscussionsSuccess(discussions);
     }
   }
--- lib/src/discussions_list/discussions_list_ui.dart
+++ lib/src/discussions_list/discussions_list_ui.dart
@@ -1,38 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_chat/src/group_management/group_management.dart';
+import 'package:flutter_chat/src/models/user.dart';
+import 'package:flutter_chat/src/repositories/firebase_repository.dart';
+
+import 'discussions_list.dart';
 
 class DiscussionsListPage extends StatefulWidget {
+  final FirebaseRepository firebaseRepository;
+  final GroupManagementBloc groupManagementBloc;
+  final User currentUser;
+
+  DiscussionsListPage(
+      this.firebaseRepository, this.groupManagementBloc, this.currentUser)
+      : assert(firebaseRepository != null),
+        assert(currentUser != null);
+
   @override
   _DiscussionsListPageState createState() => _DiscussionsListPageState();
 }
 
 class _DiscussionsListPageState extends State<DiscussionsListPage> {
+  DiscussionsListBloc _discussionsListBloc;
   @override
   void initState() {
     super.initState();
+    _discussionsListBloc = DiscussionsListBloc(widget.firebaseRepository,
+        widget.groupManagementBloc, widget.currentUser.id);
+    _discussionsListBloc.dispatch(GetDiscussionsList());
   }
 
   @override
   Widget build(BuildContext context) {
-    Map<String, dynamic> discussions = {
-      "id1": {
-        "title": "Title group 1",
-        "lastMsg": "Bonjour à tous",
-        "updatedAt": "2019-04-18T15:23:19.300035"
-      },
-      "id2": {
-        "title": "Title group 1",
-        "lastMsg": "Bonjour à tous",
-        "updatedAt": "2019-04-18T15:24:19.300035"
-      },
-    };
-    List<String> keys = discussions.keys.toList();
-    return ListView.builder(
-        itemCount: discussions.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(discussions[keys[index]]['title']),
-            subtitle: Text(discussions[keys[index]]['lastMsg']),
-          );
+    return BlocBuilder<DiscussionsListEvent, DiscussionsListState>(
+        bloc: _discussionsListBloc,
+        builder: (context, discussionsListState) {
+          if (discussionsListState is DiscussionsInitial) {
+            return Container();
+          }
+          if (discussionsListState is DiscussionsLoading) {
+            return Center(
+              child: SizedBox(
+                width: 20,
+                height: 20,
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+          if (discussionsListState is DiscussionsSuccess) {
+            if (discussionsListState.discussions.length > 0) {
+              List<String> keys =
+                  discussionsListState.discussions.keys.toList();
+              return ListView.builder(
+                  itemCount: keys.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(discussionsListState.discussions[keys[index]]
+                          ['title']),
+                      subtitle: Text(
+                        discussionsListState.discussions[keys[index]]
+                            ['lastMsg'],
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      trailing: IconButton(
+                          icon: Icon(Icons.message),
+                          onPressed: () {
+                            _discussionsListBloc.groupManagementBloc
+                                .dispatch(NavigateToGroup(keys[index]));
+                          }),
+                    );
+                  });
+            } else {
+              Center(
+                child: Text('No discussions available'),
+              );
+            }
+          }
         });
   }
 }
