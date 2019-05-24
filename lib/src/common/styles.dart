c528001039cdeeb4a92b0ec4163e54e9a8a46809
MattisBrizardOAB <mattis.brizard@orange.com>
2019-05-24 11:17:51 +0200

    added styles file

---
+++ lib/src/common/styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static final Color mainColor = Color.fromRGBO(90, 95, 129, 1);
+  static final Color otherMessageBackgroundColor = Colors.black12;
+  static const TextStyle otherMessagetext =
+      TextStyle(color: Colors.black, fontSize: 14);
+  static const TextStyle myMessagetext =
+      TextStyle(color: Colors.white, fontSize: 14);
+}
