9e4eabd39d6e0767fd1da83e5a907e881a9001ee
Mattis Brizard <matttis.brizard@orange.com>
2019-07-16 11:26:25 +0200

    added  discussion model

---
+++ lib/src/models/discussion.dart
@@ -0,0 +1,12 @@
+import 'package:meta/meta.dart';
+
+class Discussion {
+  Discussion({
+    @required this.groupId,
+    @required this.title,
+    @required this.lastMessage,
+  });
+  final String groupId;
+  final String title;
+  final String lastMessage;
+}
