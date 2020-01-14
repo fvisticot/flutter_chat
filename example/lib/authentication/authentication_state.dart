a2afa5528e6fc79e2a27d060a6c57a3aee2a420e
Mattis Brizard <matttis.brizard@orange.com>
2020-01-14 11:09:25 +0100

    updated dependencies & v0.1.2

--- example/lib/authentication/authentication_state.dart
+++ example/lib/authentication/authentication_state.dart
@@ -1,23 +1,34 @@
 import 'package:equatable/equatable.dart';
 
-abstract class AuthenticationState extends Equatable {}
+abstract class AuthenticationState extends Equatable {
+  const AuthenticationState();
+}
 
 class AuthenticationUninitialized extends AuthenticationState {
+  @override
+  List<Object> get props => [];
+
   @override
   String toString() => 'AuthenticationUninitialized';
 }
 
 class AuthenticationAuthenticated extends AuthenticationState {
   @override
+  List<Object> get props => [];
+  @override
   String toString() => 'AuthenticationAuthenticated';
 }
 
 class AuthenticationUnauthenticated extends AuthenticationState {
   @override
+  List<Object> get props => [];
+  @override
   String toString() => 'AuthenticationUnauthenticated';
 }
 
 class AuthenticationLoading extends AuthenticationState {
+  @override
+  List<Object> get props => [];
   @override
   String toString() => 'AuthenticationLoading';
 }
